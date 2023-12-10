c0a5738ef1338a514ced3f9a0fb249c4815ecec3
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-10 20:58:56 +0530

    about for loop

---
+++ src/program/ForLoop.java
@@ -0,0 +1,10 @@
+package program;
+
+public class ForLoop {
+	 public static void main(String[] args) {
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.print(i + " ");
+	        }
+	        System.out.println();
+	    }
+}
