7d13d25ec59738c3184b8a30564412752bad1336
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-10 20:58:10 +0530

    Program on array.

---
+++ src/program/Arrays.java
@@ -0,0 +1,13 @@
+package program;
+
+public class Arrays {
+	 public static void main(String[] args) {
+	        int[] numbers = { 1, 2, 3, 4, 5 };
+
+	        System.out.println("Elements in the array:");
+	        for (int number : numbers) {
+	            System.out.print(number + " ");
+	        }
+	        System.out.println();
+	    }
+}
