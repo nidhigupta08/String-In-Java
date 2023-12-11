1d8a7c21b54f4795e8b6ebf25de61b9a906eae7d
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-11 20:28:23 +0530

    program on function

---
+++ src/program/Function.java
@@ -0,0 +1,12 @@
+package program;
+
+public class Function {
+	public static void main(String[] args) {
+        int result = addNumbers(5, 7);
+        System.out.println("Result: " + result);
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
