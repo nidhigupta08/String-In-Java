31109bad2e157cc1ff667fc9ae002655738b27fc
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-25 23:08:17 +0530

    program on datatypes

---
+++ src/program/DataTypes.java
@@ -0,0 +1,31 @@
+package program;
+
+public class DataTypes {
+	 public static void main(String[] args) {
+	        // Integer types
+	        byte myByte = 127; // Stores whole numbers from -128 to 127
+	        short myShort = 32767; // Stores whole numbers from -32768 to 32767
+	        int myInt = 2147483647; // Stores whole numbers from -2147483648 to 2147483647
+	        long myLong = 9223372036854775807L; // Stores very large whole numbers
+
+	        // Floating-point types
+	        float myFloat = 3.14159f; // Stores single-precision floating-point numbers
+	        double myDouble = 2.71828; // Stores double-precision floating-point numbers
+
+	        // Character type
+	        char myChar = 'A'; // Stores a single character
+
+	        // Boolean type
+	        boolean myBoolean = true; // Stores true or false values
+
+	        // Printing the values
+	        System.out.println("Byte value: " + myByte);
+	        System.out.println("Short value: " + myShort);
+	        System.out.println("Integer value: " + myInt);
+	        System.out.println("Long value: " + myLong);
+	        System.out.println("Float value: " + myFloat);
+	        System.out.println("Double value: " + myDouble);
+	        System.out.println("Character value: " + myChar);
+	        System.out.println("Boolean value: " + myBoolean);
+	    }
+}
