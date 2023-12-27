234970c0e3303811062de761c7a8b2c75a45f467
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-27 23:19:28 +0530

    multiplication

---
+++ src/program/MultiplicationTable.java
@@ -0,0 +1,15 @@
+package program;
+
+public class MultiplicationTable {
+	public static void main(String[] args) {
+        int tableSize = 10; // Define the size of the multiplication table
+        
+        // Nested loop to generate the multiplication table
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                System.out.print(i * j + "\t"); // Print the product of i and j
+            }
+            System.out.println(); // Move to the next line after printing each row
+        }
+    }
+}
