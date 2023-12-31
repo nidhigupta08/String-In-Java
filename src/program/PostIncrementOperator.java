ca052a33f7fea8d8c22d8f00c8c06847c264dd3e
Nidhi Gupta <guptanidhi8000@gmail.com>
2023-12-31 22:26:42 +0530

    program

--- src/program/PostIncrementOperator.java
+++ src/program/PostIncrementOperator.java
@@ -4,7 +4,9 @@ public class PostIncrementOperator {
 	public static void main(String[] args) {
 
 		int i = 5;
-
+//i++(i will increment by 1 but it will return the current value)
+//++i (i value is 6 and increment by 1 and then returns the updated value. )
+//i--(i=7,first i decerement by 1 but returns the current value of i before the decrement.) 
 		System.out.println(i++ + ++i * i--);
 
 		}
--- src/program/PreIncrementOperator.java
+++ src/program/PreIncrementOperator.java
@@ -4,7 +4,9 @@ public class PreIncrementOperator {
 	public static void main(String[] args) {
 
 		int x = 10;
-
+// 10+{10-(10*2)}+(++10)
+//10+{10-20} +(11)
+//10+1 =11
 		x += x-(x * 2) + (++x);
 
 		System.out.println(x);
