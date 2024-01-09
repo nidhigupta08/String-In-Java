eb20b484faa7f3648d0ad5b96971c662cf4d0da9
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-01-09 22:01:10 +0530

    program on ASCII value

---
+++ src/program/AsciiValue.java
@@ -0,0 +1,9 @@
+package program;
+
+public class AsciiValue {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int asciiValue = ch;
+        System.out.println("ASCII value of " + ch + " is: " + asciiValue);
+    }
+}
