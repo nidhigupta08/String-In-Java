60498ccd7ae29de287f9a24faf15b5535e8f9e5d
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-01-13 21:39:34 +0530

    program on break statement using nested for loop.

---
+++ src/program/BreakNestedLoopExample.java
@@ -0,0 +1,14 @@
+package program;
+
+public class BreakNestedLoopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (i == 2 && j == 2) {
+                    break;  // Terminates only the inner loop
+                }
+                System.out.println("i = " + i + ", j = " + j);
+            }
+        }
+    }
+}
