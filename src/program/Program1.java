d2bd3f9fb4071e83ca484ed10eb6084706d4428d
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-01-18 21:14:11 +0530

    program on string

---
+++ src/program/Program1.java
@@ -0,0 +1,15 @@
+package program;
+
+public class Program1 {
+	public static void main(String args[]) {
+	String str1 = "Java";
+
+	String str2 = "Java";
+
+	String str3 = new String("Java");
+
+	System.out.println(str1 == str2);
+
+	System.out.println(str1 == str3);
+}
+	}
