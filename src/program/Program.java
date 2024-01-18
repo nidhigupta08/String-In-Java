666596ffa381400de97646ccf436268a4048b98b
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-01-18 21:12:51 +0530

    program on increment and decrement.

---
+++ src/program/Program.java
@@ -0,0 +1,13 @@
+package program;
+
+public class Program {
+
+		public static void main(String[] args) {
+
+		int x = 5;
+
+		int result = x++ + ++x*2+x-- - --x; 
+		System.out.println(result);
+		}
+}
+
