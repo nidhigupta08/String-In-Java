276426441be1da8f7ebe3a65640b2bc1e184b41b
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-01-19 21:48:04 +0530

    Different set of classes demonstrating the IS-A relationship using inheritance

---
+++ src/program/IS_A_Relationship1.java
@@ -0,0 +1,40 @@
+package program;
+
+class Vehicle3 {
+    String brand;
+
+    public Vehicle3(String brand) {
+        this.brand = brand;
+    }
+
+    public void start() {
+        System.out.println(brand + " is starting");
+    }
+
+    public void stop() {
+        System.out.println(brand + " is stopping");
+    }
+}
+
+class Carr extends Vehicle3 {
+    int numberOfDoors;
+
+    public Carr(String brand, int numberOfDoors) {
+        super(brand);
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public void drive() {
+        System.out.println(brand + " with " + numberOfDoors + " doors is driving");
+    }
+}
+
+public class IS_A_Relationship1 {
+    public static void main(String[] args) {
+        Carr myCar = new Carr("Toyota", 4);
+
+        myCar.start();      // Inherited from Vehicle
+        myCar.drive();      // Specific to Car
+        myCar.stop();       // Inherited from Vehicle
+    }
+}
