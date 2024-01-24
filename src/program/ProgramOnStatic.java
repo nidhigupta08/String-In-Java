55cf64551e3f9d54b7c057202c289fbc1b1dc83a
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-01-24 20:42:30 +0530

    program on static

---
+++ src/program/ProgramOnStatic.java
@@ -0,0 +1,13 @@
+package program;
+
+public class ProgramOnStatic {
+public static void main(String[] atgs) {
+//	ProgramOnStatic s=new ProgramOnStatic();
+//	s.class1();
+	class1();
+}
+
+public static void class1() {
+	System.out.println("Nidhi");
+	}
+}
