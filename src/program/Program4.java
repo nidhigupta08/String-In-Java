cecf1f609222152a3b264a30524a09754180009b
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-02-01 21:31:09 +0530

    program

---
+++ src/program/Program4.java
@@ -0,0 +1,9 @@
+package program;
+
+public class Program4 {
+	public static void main(String[] args){
+		 int n = 5;
+		 for (int i = 0, j = n - 1; i < n; i++, j--)
+		 System.out.print(i + "" + j + " ");
+		 }
+}
