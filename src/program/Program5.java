a22065b7a37c69408704816c093d2551237a46eb
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-02-01 21:32:18 +0530

    Array program

---
+++ src/program/Program5.java
@@ -0,0 +1,12 @@
+package program;
+
+public class Program5 {
+	public static void main(String[] args) {
+		 int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		 int sum = 0;
+		 for (int i = 0; i < matrix.length; i++) {
+		 sum += matrix[i][i];
+		 }
+		 System.out.println(sum);
+		 }
+}
