355f875e8a26c29b70ad8877bed2cb190de3972c
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-02-08 22:50:00 +0530

    program on bubble sort

---
+++ src/program/BubbleSort1.java
@@ -0,0 +1,49 @@
+package program;
+
+import java.util.Scanner;
+
+public class BubbleSort1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Array before sorting:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("\nArray after sorting:");
+        printArray(arr);
+
+        scanner.close();
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
