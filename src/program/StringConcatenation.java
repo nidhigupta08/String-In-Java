da3ea8980d2e67b436ebd2f6f4b81cb83f2f38f0
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-02-18 18:06:31 +0530

    program on concatenation

---
+++ src/program/StringConcatenation.java
@@ -0,0 +1,15 @@
+package program;
+
+public class StringConcatenation {
+	 public static void main(String[] args) {
+	        // Define two strings
+	        String firstString = "Good";
+	        String secondString = "Morning";
+
+	        // Concatenate strings using the concat() method
+	        String concatenatedString = firstString.concat(" ").concat(secondString);
+
+	        // Display the concatenated string
+	        System.out.println("Concatenated String: " + concatenatedString);
+	    }
+}
