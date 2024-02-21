85807adce87d26d5e84e829ff33a63a386a2f69a
Nidhi Gupta <guptanidhi8000@gmail.com>
2024-02-21 21:56:32 +0530

    Implements binary search algorithm recursively.

---
+++ src/program/BinarySearchRecursive.java
@@ -0,0 +1,32 @@
+package program;
+
+public class BinarySearchRecursive {
+    public static int binarySearch(int[] arr, int target, int left, int right) {
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                return binarySearch(arr, target, mid + 1, right);
+            } else {
+                return binarySearch(arr, target, left, mid - 1);
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
+
+        int index = binarySearch(arr, target, 0, arr.length - 1);
+
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index: " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
+        }
+    }
+}
