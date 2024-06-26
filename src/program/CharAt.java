f3f0b2ee9cbacba095a94424d7faf9427ea25b29
Nidhi <guptanidhi8000@gmail.com>
2024-06-26 22:52:54 +0530

    small program on charAt() method.

---
+++ src/program/CharAt.java
@@ -0,0 +1,9 @@
+package program;
+
+public class CharAt {
+	 public static void main(String[] args) {
+	        String str = "Hello, World!";
+	        char ch = str.charAt(6);
+	        System.out.println("Character at index 6: " + ch);
+	    }
+}
