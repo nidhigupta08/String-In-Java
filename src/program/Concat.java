8b87f10231f01465f48ed33ab5eca81cf68a0437
Nidhi <guptanidhi8000@gmail.com>
2024-06-26 22:53:32 +0530

    Small program using concatenation method   concat()

---
+++ src/program/Concat.java
@@ -0,0 +1,10 @@
+package program;
+
+public class Concat {
+	  public static void main(String[] args) {
+	        String str1 = "Hello, ";
+	        String str2 = "World!";
+	        String result = str1.concat(str2);
+	        System.out.println("Concatenated string: " + result);
+	    }
+}
